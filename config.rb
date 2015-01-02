cc326314d1ccdeec5e95577535d3ae66ef41624a
Justin Campbell <justin@justincampbell.me>
2015-01-02 10:14:23 -0500

    Basic data for books and episodes

--- config.rb
+++ config.rb
@@ -4,3 +4,20 @@ set :js_dir, 'javascripts'
 
 configure :build do
 end
+
+helpers do
+  def podcast_name
+    "CS Book Club"
+  end
+
+  def podcast_description
+    "Computer Science for Everyone"
+  end
+
+  def title
+    [
+      podcast_name,
+      current_page.data.title || yield_content(:title)
+    ].compact.join(" - ")
+  end
+end
